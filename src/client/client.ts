bd11c697e91f54921863a6d610aed5a83984a9a2
dmitrijbykov <dmitryglitch@gmail.com>
2022-03-23 15:52:18 +0300

    Add dist

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,7 +1,9 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
+import {Color} from "three";
 
 const scene = new THREE.Scene()
+scene.background = new Color("#3474eb")
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 camera.position.z = 2
@@ -12,16 +14,17 @@ document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
 
+const loader = new THREE.TextureLoader();
+const texture = loader.load("/textures/strike_red.png");
+
 const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-})
+const material = new THREE.MeshBasicMaterial({map: texture})
 
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
 window.addEventListener('resize', onWindowResize, false)
+
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
@@ -43,4 +46,5 @@ function animate() {
 function render() {
     renderer.render(scene, camera)
 }
+
 animate()
